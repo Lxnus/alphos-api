6190c16aae6ee4e34aad43fa5b02a841c210f5ad
lxnus <lxnusofficial@gmail.com>
2021-08-06 22:29:32 +0200

    recode socket read

--- src/main/java/net/alphos/api/internal/DefaultAlphosApiClient.java
+++ src/main/java/net/alphos/api/internal/DefaultAlphosApiClient.java
@@ -7,15 +7,16 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
-import java.nio.channels.CompletionHandler;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
 @Singleton
 public class DefaultAlphosApiClient implements AlphosApiClient {
 
-  private final String host = "service.alphos.dev";
-  private final int port = 222;
+  private final String host = "127.0.0.1";//"service.alphos.dev";
+  private final int port = 80;//222;
 
   private AsynchronousSocketChannel client;
 
@@ -24,42 +25,23 @@ public class DefaultAlphosApiClient implements AlphosApiClient {
     try {
       client = AsynchronousSocketChannel.open();
       InetSocketAddress socketAddress = new InetSocketAddress(host, port);
-      client.connect(socketAddress, client, new CompletionHandler<Void, AsynchronousSocketChannel>() {
-        @Override
-        public void completed(Void result, AsynchronousSocketChannel attachment) {}
-
-        @Override
-        public void failed(Throwable exc, AsynchronousSocketChannel attachment) {
-          System.err.println("Failed to connect to server.");
-          System.err.println("Possible reasons: The hostname is unknown.");
-          System.err.println("- The hostname is unknown.");
-          System.err.println("- The service is currently not available. (see: https://status.alphos.dev");
-          exc.printStackTrace();
-        }
-      });
-    } catch (IOException e) {
+      Future<Void> future = client.connect(socketAddress);
+      future.get();
+    } catch (IOException | ExecutionException | InterruptedException e) {
       e.printStackTrace();
     }
   }
 
   @Override
   public void send(String message) {
-    write(client, message);
-  }
-
-  private void write(final AsynchronousSocketChannel channel, final String message) {
-    ByteBuffer buffer = ByteBuffer.allocate(2048);
-    buffer.put(message.getBytes(StandardCharsets.UTF_8));
-    buffer.flip();
-    channel.write(buffer, channel, new CompletionHandler<Integer, AsynchronousSocketChannel>() {
-      @Override
-      public void completed(Integer result, AsynchronousSocketChannel attachment) {}
-
-      @Override
-      public void failed(Throwable exc, AsynchronousSocketChannel attachment) {
-        exc.printStackTrace();
-      }
-    });
+    byte[] bytes = message.getBytes(StandardCharsets.UTF_8);
+    ByteBuffer buffer = ByteBuffer.wrap(bytes);
+    Future<Integer> result = client.write(buffer);
+    try {
+      result.get();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
